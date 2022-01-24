a91ebde79c0645207d11dfebb80939c01db8067f
Ronald L. Sprouse <ronald@berkeley.edu>
2022-01-24 13:54:27 -0800

    Handling select2

--- assets/js/cla-select2.js
+++ assets/js/cla-select2.js
@@ -124,7 +124,7 @@ const handleSelect2FormSubmit = event => {
   // Stop the form from submitting since we'll POST from here.
   event.preventDefault();
   tab = coll_or_bndl_tab();
-  var query = get_query_from_form(event.currentTarget.id);
+  var query = get_query_from_form('cla-search-form');
   $.ajax({
       method: 'POST',
       url: aws_endpoint + 'sq',
